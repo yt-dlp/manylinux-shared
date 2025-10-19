9584d0a7d74337272c02e96073096f5a818de83d
bashonly <bashonly@protonmail.com>
2025-10-19 18:10:40 -0500

    Adjust workflow, build scripts & Dockerfile for cross-compilation

--- docker/build_scripts/build-shared-cpython.sh
+++ docker/build_scripts/build-shared-cpython.sh
@@ -17,8 +17,6 @@ CERT_OIDC_ISSUER=$2
 CPYTHON_VERSION=$3
 CPYTHON_DOWNLOAD_URL=https://www.python.org/ftp/python
 
-PREFIX="/opt/shared-cpython-$(echo "${CPYTHON_VERSION}" | awk -F "." '{printf "%d.%d", $1, $2}')"
-OPENSSL_PREFIX="/opt/_internal/openssl-3.5"
 
 function pyver_dist_dir {
 	# Echoes the dist directory name of given pyver, removing alpha/beta prerelease
@@ -35,8 +33,9 @@ cosign  verify-blob "Python-${CPYTHON_VERSION}.tar.xz" --bundle "Python-${CPYTHO
 
 tar -xJf "Python-${CPYTHON_VERSION}.tar.xz"
 pushd "Python-${CPYTHON_VERSION}"
+PREFIX="/opt/shared-cpython-$(echo "${CPYTHON_VERSION}" | awk -F "." '{printf "%d.%d", $1, $2}')"
 mkdir -p "${PREFIX}/lib"
-CFLAGS_EXTRA=""
+LDFLAGS_EXTRA="-Wl,-rpath=${PREFIX}/lib"
 CONFIGURE_ARGS=(
 	--enable-shared
 	--enable-ipv6
@@ -50,7 +49,7 @@ CONFIGURE_ARGS=(
 # https://github.com/python/cpython/commit/6146295a5b8e9286ccb8f90818b764c9a0192090
 # https://github.com/python/cpython/issues/90548
 # https://github.com/python/cpython/pull/138348
-if [ "${BASE_POLICY}" == "manylinux" ] || [ "${CPYTHON_VERSION::5}" == "3.14." ]; then
+if [ "${BASE_POLICY}" == "manylinux" ] || [ "${CPYTHON_VERSION:0:5}" == "3.14." ]; then
 	CONFIGURE_ARGS+=(
 		--enable-optimizations
 		--with-computed-gotos
@@ -65,7 +64,7 @@ fi
 
 if [ "${AUDITWHEEL_POLICY}" == "manylinux2014" ] ; then
 	# Python 3.11+
-	export TCLTK_LIBS="-ltk8.6 -ltcl8.6"
+	export TCLTK_LIBS="-L/usr/local/lib -ltk8.6 -ltcl8.6"
 fi
 
 if [ "${BASE_POLICY}_${AUDITWHEEL_ARCH}" == "manylinux_armv7l" ]; then
@@ -75,27 +74,23 @@ elif [ "${BASE_POLICY}_${AUDITWHEEL_ARCH}" == "musllinux_armv7l" ]; then
 fi
 
 case "${CPYTHON_VERSION}" in
-	3.8.*|3.9.*|3.10.*) sed -i "s|/usr/local/include/sqlite3|/opt/_internal/sqlite3/include|g ; s|sqlite_extra_link_args = ()|sqlite_extra_link_args = ('-Wl,--enable-new-dtags,-rpath=/opt/_internal/sqlite3/lib',)|g" setup.py;;
+	3.8.*|3.9.*|3.10.*) sed -i "s|/usr/local/include/sqlite3|/opt/_internal/sqlite3/include|g ; s|sqlite_extra_link_args = ()|sqlite_extra_link_args = ('-Wl,-rpath=/opt/_internal/sqlite3/lib',)|g" setup.py;;
 	*) ;;
 esac
 
-CONFIGURE_ARGS+=("--with-openssl=${OPENSSL_PREFIX}")
-case "${CPYTHON_VERSION}" in
-	3.8.*|3.9.*) export LD_RUN_PATH=${OPENSSL_PREFIX}/lib;;
-	*) CONFIGURE_ARGS+=(--with-openssl-rpath=auto);;
-esac
+OPENSSL_PREFIX="/opt/_internal/openssl-3.5"
+CONFIGURE_ARGS+=(
+	"--with-openssl=${OPENSSL_PREFIX}"
+	--with-openssl-rpath=auto
+)
 
 unset _PYTHON_HOST_PLATFORM
-
-if [ "${AUDITWHEEL_ARCH}" == "x86_64" ] && echo | gcc -S -x c -v - 2>&1 | grep 'march=x86-64-v' > /dev/null; then
-	export EXTRA_CFLAGS="-mtune=generic -march=x86-64"
-fi
 
 # configure with hardening options only for the interpreter & stdlib C extensions
 # do not change the default for user built extension (yet?)
 ./configure \
-	CFLAGS_NODIST="${MANYLINUX_CFLAGS} ${MANYLINUX_CPPFLAGS} ${CFLAGS_EXTRA}" \
-	LDFLAGS_NODIST="${MANYLINUX_LDFLAGS} -Wl,-rpath=${PREFIX}/lib" \
+	CFLAGS_NODIST="${MANYLINUX_CFLAGS} ${MANYLINUX_CPPFLAGS}" \
+	LDFLAGS_NODIST="${MANYLINUX_LDFLAGS} ${LDFLAGS_EXTRA}" \
 	"--prefix=${PREFIX}" "${CONFIGURE_ARGS[@]}" > /dev/null
 make > /dev/null
 make install > /dev/null
